342ae01e26abee0e9317cbd317345aa96ff6a69d
miacuesta <49257077@est.ort.edu.ar>
2026-03-13 14:05:56 -0300

    g

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("taskInput")
+const button = document.getElementById("addBtn")
+const list = document.getElementById("taskList")
+
+button.addEventListener("click", () => {
+
+  const li = document.createElement("li")
+  li.textContent = input.value
+
+  list.appendChild(li)
+
+  input.value = ""
+
+})
